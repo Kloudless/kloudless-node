60dfaf21eebd2148eb3c95393b60763c932395ab
Leo Zhang <leo@leozhang.me>
2014-09-14 18:13:08 -0700

    Fix multipart upload session URL.

--- test/index.js
+++ test/index.js
@@ -23,7 +23,7 @@ async.waterfall([
       }
       accountId = res.objects[0].id;
       console.log('accounts base test pass');
-      console.log('accounts:', res.objects);
+      // console.log('accounts:', res.objects);
       cb(null);
     });
   },
